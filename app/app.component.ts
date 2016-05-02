3d90b63081799e0470870cfd5f2f8396bbccd390
Entaro Adun <dcwmark@gmail.com>
2016-05-01 23:49:54 -0400

    Code copied.

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+/**
+ * angularjs-app/app/app.component.ts
+**/
+
+/* Importing Component decorator function from anularjs library module */
+ 
+import {Component} from 'angular2/core'
+ 
+ 
+/* Creating  AppComponent Class decorator */
+@Component ({
+	selector: 'learning-turn',
+	template: '<h1>AngularJS 2 Hello World Applicaiton: LearningTurn</h1>'
+})
+ 
+/* Creating AppComponent class */
+/* Exporting AppComponent Class */
+export class AppComponent{}
+
